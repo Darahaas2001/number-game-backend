574be8f8ff117bbe06cfc2d534cca48a61212420
Darahaas N <65232365+Darahaas2001@users.noreply.github.com>
2022-09-02 11:42:23 +0530

    initial commit

---
+++ User.js
@@ -0,0 +1,119 @@
+let users = [];
+let selectedNumbers = [];
+
+const addUser = ({ id, name, room, color, userNumber }) => {
+	name = name.trim().toLowerCase();
+	room = room.trim().toLowerCase();
+
+	const existingUser = users.find(
+		(user) => user.room === room && user.name === name
+	);
+
+	if (existingUser) {
+		return { error: 'Username is taken' };
+	}
+	const user = {
+		id,
+		name,
+		room,
+		color,
+		userNumber,
+		eliminated: false,
+		active: false,
+	};
+
+	users.push(user);
+	return { user };
+};
+
+const removeUser = (id) => {
+	const index = users.findIndex((user) => {
+		user.id === id;
+	});
+
+	if (index !== -1) {
+		return users.splice(index, 1)[0];
+	}
+};
+
+const getUser = (id) => users.find((user) => user.id === id);
+const getUsersInGame = (room) =>
+	users.filter((user) => user.room === room && user.eliminated === false);
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+const eliminateUserInRoom = (number, room) => {
+	let filteredData = users.filter((user) => user.room === room);
+
+	for (const data of filteredData) {
+		if (data.userNumber === number) {
+			data.eliminated = true;
+		}
+	}
+	return filteredData;
+};
+const activeUserInRoom = (chance, room) => {
+	if (chance <= getUsersInGame(room).length - 1) {
+		return { status: 'SUCCESS', payload: chance };
+	} else {
+		return 'FAIL';
+	}
+};
+const deactiveUserInRoom = (id, room) => {
+	let index = users.findIndex(
+		(user) => user.id === id && user.room === room && user.active === true
+	);
+	if (index !== -1) {
+		users[index]['active'] = false;
+		return users;
+	}
+};
+
+const selectNumber = (id, room, number, color) => {
+	let userIndex = users.filter((user) => user.userNumber === number);
+
+	if (userIndex.length > 0) {
+		console.log(userIndex);
+
+		return { payload: 'USER_ELIMINATION', userIndex };
+	}
+
+	let selectedNumIndex = selectedNumbers.findIndex(
+		(user) => user.id === id && user.room === room
+	);
+
+	if (selectedNumIndex !== -1) {
+		selectedNumbers[selectedNumIndex]['nums'] = new Set([
+			...selectedNumbers[selectedNumIndex]['nums'],
+			number,
+		]);
+		return { payload: 'SUCCESS', color, number };
+	}
+	selectedNumbers.push({
+		id,
+		room,
+		nums: [number],
+		color,
+	});
+	return { payload: 'SUCCESS', color, number };
+};
+
+const selectNumberOfUser = (id, room) => {
+	let selectedNumUser = selectedNumbers.find(
+		(user) => user.id === id && user.room === room
+	);
+	return selectedNumUser;
+};
+
+module.exports = {
+	addUser,
+	removeUser,
+	getUser,
+	getUsersInRoom,
+	selectNumber,
+	selectNumberOfUser,
+	getUsersInGame,
+	eliminateUserInRoom,
+	activeUserInRoom,
+	deactiveUserInRoom,
+};
---
+++ index.js
@@ -0,0 +1,110 @@
+const { Server } = require('socket.io');
+const {
+	addUser,
+	getUsersInRoom,
+	selectNumber,
+	getUsersInGame,
+	eliminateUserInRoom,
+	activeUserInRoom,
+	deactiveUserInRoom,
+	removeUser,
+} = require('./User');
+const io = new Server(process.env.PORT || 8000, {
+	cors: { origin: '*', allowedHeaders: ['Content-Type: application/json'] },
+});
+let chance = 0;
+
+io.on('connection', (socket) => {
+	socket.on('disconnect', (reason) => {
+		console.log(socket.id + ' disconnected');
+		removeUser(socket.id);
+	});
+	socket.on('room-add', (data1) => {
+		let data = JSON.parse(data1);
+		socket.join(data.room);
+
+		let resp = addUser({
+			id: socket.id,
+			name: data.username,
+			room: data.room,
+			color: data.color,
+			userNumber: data.userNumber,
+		});
+		if (resp.error) {
+			return socket.emit('error', 'Invalid Username, try something else');
+		}
+		let userRoomData = getUsersInGame(data.room);
+
+		io.to(data.room).emit('updateUserData', JSON.stringify(userRoomData));
+		let activeUserData = activeUserInRoom(chance, data.room);
+		io.to(data.room).emit(
+			'updateActivePlayer',
+			JSON.stringify(activeUserData.payload)
+		);
+
+		io.to(userRoomData[chance].id).emit('enableMouse', true);
+	});
+	socket.on('selectNumber', (selectedNumData) => {
+		let data = JSON.parse(selectedNumData);
+
+		let resp = selectNumber(data.id, data.room, data.number, data.color);
+
+		if (resp.payload === 'USER_ELIMINATION') {
+			/**
+			 * Code for striking the user name
+			 * Should Mark the block Black
+			 */
+			resp.userIndex.forEach((user) => {
+				io.to(user.id).emit('disableMouse', true);
+			});
+			io.to(data.room).emit('mark-black', data.number);
+
+			/** Remove the User chance
+			 *
+			 */
+			let chanceResp = activeUserInRoom(chance + 2, data.room);
+			if (chanceResp.status === 'SUCCESS') {
+				io.to(data.room).emit(
+					'updateActivePlayer',
+					JSON.stringify(chanceResp.payload)
+				);
+			} else if (chanceResp === 'FAIL') {
+				chance = 0;
+			}
+			let userRoomData = eliminateUserInRoom(data.number, data.room);
+
+			io.to(data.room).emit('updateUserData', JSON.stringify(userRoomData));
+		} else if (resp.payload === 'SUCCESS') {
+			socket.emit('disableMouse', true);
+			chance += 1;
+			io.to(data.room).emit(
+				'oppoColor',
+				JSON.stringify({ color: resp.color, number: resp.number })
+			);
+			let chanceResp = activeUserInRoom(chance, data.room);
+			if (chanceResp.status === 'SUCCESS') {
+				io.to(data.room).emit(
+					'updateActivePlayer',
+					JSON.stringify(chanceResp.payload)
+				);
+				let userData = getUsersInGame(data.room);
+
+				io.to(userData[chance]['id']).emit('enableMouse', true);
+				//	let deactiveData = deactiveUserInRoom(data.id, data.room);
+
+				//	io.to(data.room).emit('updateUserData', JSON.stringify(deactiveData));
+				//console.log(chanceResp.payload);
+			} else if (chanceResp === 'FAIL') {
+				chance = 0;
+				let chanceResp = activeUserInRoom(chance, data.room);
+				let userData = getUsersInGame(data.room);
+				io.to(data.room).emit(
+					'updateActivePlayer',
+					JSON.stringify(chanceResp.payload)
+				);
+				io.to(userData[chance]['id']).emit('enableMouse', true);
+			}
+		}
+		//console.log(selectNumberOfUser(data.id, data.room));
+	});
+});
